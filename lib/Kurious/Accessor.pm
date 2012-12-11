93f5eaca8b19a065b8650ade5cc8da3d9dc987a2
Craftworks <craftworks.jp+dev@gmail.com>
2012-12-11 10:48:14 +0900

    added enable_utf8(): utility method for flagged utf8

--- lib/Kurious/Accessor.pm
+++ lib/Kurious/Accessor.pm
@@ -1,7 +1,16 @@
 package Kurious::Accessor;
+
 use Mojo::Base -base;
+use Data::Recursive::Encode;
+
 sub log       { shift->app->log }
 sub config    { shift->app->config }
 sub accessor  { shift->app->accessor(@_) }
 sub driver    { shift->app->driver(@_) }
+
+sub enable_utf8 {
+    my ($self, $rows) = @_;
+    Data::Recursive::Encode->decode_utf8($rows);
+}
+
 1;
