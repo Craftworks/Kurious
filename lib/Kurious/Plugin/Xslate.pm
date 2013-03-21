6f655bb7a96b6030ac47915f0f7bce51114a1b12
Craftworks <craftworks.jp+dev@gmail.com>
2013-03-21 14:31:08 +0900

    cleanup code

--- lib/Kurious/Plugin/Xslate.pm
+++ lib/Kurious/Plugin/Xslate.pm
@@ -3,6 +3,7 @@ package Kurious::Plugin::Xslate;
 use Kurious::Base 'Mojolicious::Plugin';
 use MojoX::Renderer::Xslate;
 use Text::Xslate qw(html_builder mark_raw);
+use Class::Inspector;
 use HTML::FillInForm;
 use HTML::Packer;
 use JavaScript::Minifier::XS 'minify';
@@ -16,61 +17,71 @@ sub register {
 
     $template_options->{'module'} = ['Text::Xslate::Bridge::Star'];
 
-    my $fif = HTML::FillInForm->new;
-    $template_options->{'function'} = +{
-        'css' => sub {
-            my $href = shift;
-            return mark_raw(qq{<link rel="stylesheet" href="$href">});
-        },
-        'script' => sub {
-            my $src = shift;
-            return mark_raw(qq{<script src="$src"></script>});
-        },
-        'nl2br' => sub {
-            my $str = shift;
-            return unless defined $str and length $str;
-            $str =~ s/\x0D\x0A/<br>/go;
-            $str =~ s/[\x0D\x0A]/<br>/go;
-            return mark_raw($str);
-        },
-        'fillinform' => sub {
-            my @vars = @_;
-            return html_builder {
-                my $raw  = shift; # Text::Xslate::Type::Raw
-                my $html = $raw->as_string;
-                return $fif->fill(\$html, \@vars);
-            };
-        },
-        'query_param' => sub {
-            my ($uri, $params, $deletes) = @_;
-            my $u = URI->new($uri);
-            $u->query_param_delete($_) for (@$deletes);
-            $u->query_param(%$params);
-            return $u;
-        },
-        'minify_html' => sub {
-            state $packer = HTML::Packer->init;
-            my $opts = shift;
-            return html_builder {
-                my $raw  = shift; # Text::Xslate::Type::Raw
-                my $html = $raw->as_string;
-                return $packer->minify(\$html, $opts);
-            };
-        },
-        'minify_js' => sub {
-            return html_builder {
-                my $raw  = shift; # Text::Xslate::Type::Raw
-                my $html = $raw->as_string;
-                return minify($html);
-            };
-        },
-    };
+    # define template functions
+    for (@{ Class::Inspector->methods(ref $self, 'private') }) {
+        next unless /^__function_(\w+)$/,
+        $template_options->{'function'}{ $1 } = \&$_;
+    }
 
     my $xslate = MojoX::Renderer::Xslate->build(
         'mojo' => $app, 'template_options' => $template_options,
     );
 
     $app->renderer->add_handler('tx' => $xslate);
+}
+
+sub __function_css {
+    my $href = shift;
+    return mark_raw(qq{<link rel="stylesheet" href="$href">});
+}
+
+sub __function_script {
+    my $src = shift;
+    return mark_raw(qq{<script src="$src"></script>});
+}
+
+sub __function_nl2br {
+    my $str = shift;
+    return unless defined $str and length $str;
+    $str =~ s/\x0D\x0A/<br>/go;
+    $str =~ s/[\x0D\x0A]/<br>/go;
+    return mark_raw($str);
+}
+
+sub __function_fillinform {
+    my @vars = @_;
+    state $fif = HTML::FillInForm->new;
+    return html_builder {
+        my $raw  = shift; # Text::Xslate::Type::Raw
+        my $html = $raw->as_string;
+        return $fif->fill(\$html, \@vars);
+    };
+}
+
+sub __function_query_param {
+    my ($uri, $params, $deletes) = @_;
+    my $u = URI->new($uri);
+    $u->query_param_delete($_) for (@$deletes);
+    $u->query_param(%$params);
+    return $u;
+}
+
+sub __function_minify_html {
+    state $packer = HTML::Packer->init;
+    my $opts = shift;
+    return html_builder {
+        my $raw  = shift; # Text::Xslate::Type::Raw
+        my $html = $raw->as_string;
+        return $packer->minify(\$html, $opts);
+    };
+}
+
+sub __function_minify_js {
+    return html_builder {
+        my $raw  = shift; # Text::Xslate::Type::Raw
+        my $html = $raw->as_string;
+        return minify($html);
+    };
 }
 
 1;
