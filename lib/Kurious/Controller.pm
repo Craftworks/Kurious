967358b6f9f2a97da42c2b87afd2e3a173ca4e16
Craftworks <craftworks.jp+dev@gmail.com>
2013-05-02 12:00:25 +0900

    added controller method action_path()

--- lib/Kurious/Controller.pm
+++ lib/Kurious/Controller.pm
@@ -3,4 +3,15 @@ use Kurious::Base 'Mojolicious::Controller';
 sub log { shift->app->log }
 sub d   { shift->app->log->dump(@_) }
 
+sub action_path {
+    my $self = shift;
+
+    my $caller = (caller 3)[3];
+    my $app_class = $self->app->home->app_class;
+    my $action = substr $caller, length("$app_class\::Controller::");
+    $action =~ s{::}{/}go;
+
+    return lc $action;
+}
+
 1;
