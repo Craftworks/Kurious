d9dfd1f55d42dbdbe7b65a08c37d87a535f17f56
Craftworks <craftworks.jp+dev@gmail.com>
2013-05-03 21:05:46 +0900

    cleanup code

--- lib/Kurious/Controller.pm
+++ lib/Kurious/Controller.pm
@@ -6,7 +6,7 @@ sub d   { shift->app->log->dump(@_) }
 sub action_path {
     my $self = shift;
 
-    my $caller = (caller 3)[3];
+    my $caller = (caller 4)[3];
     my $app_class = $self->app->home->app_class;
     my $action = substr $caller, length("$app_class\::Controller::");
     $action =~ s{::}{/}go;
@@ -21,22 +21,23 @@ sub template_dir {
 sub template_renderer {
     my ($self, $template) = @_;
 
-    my %opts;
-
-    $template ||= $self->stash->{'template_name'} || $self->action_path;
-
-    $opts{'format'}   = $self->stash('format') || 'html';
-    $opts{'handler'}  = $self->stash('handler') || 'tx';
-
-    my $template_dir = $self->template_dir;
-    my $filename     = join '.', $template, @opts{qw/format handler/};
-
-    $opts{'template'} = File::Spec->catfile($template_dir, 'layout');
-    $opts{'content'}  = File::Spec->catfile($template_dir, $filename);
-
-    $opts{'env'}  = \%ENV;
-
     return sub {
+        my %opts;
+
+        $template ||= $self->stash->{'template_name'} || $self->action_path;
+
+        $opts{'format'}   = $self->stash('format') || 'html';
+        $opts{'handler'}  = $self->stash('handler') || 'tx';
+
+        my $template_dir = $self->template_dir;
+        my $filename     = join '.', $template, @opts{qw/format handler/};
+
+        $opts{'template'} = File::Spec->catfile($template_dir, 'layout');
+        $opts{'content'}  = File::Spec->catfile($template_dir, $filename);
+
+        $opts{'env'}  = \%ENV;
+
+        $self->log->debugf('Render template "%s" with "%s"', @opts{qw/template content/});
         $self->render(%opts);
     };
 }
