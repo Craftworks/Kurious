2d159f12f0232350dbffe77f23d654cb80c405b4
Craftworks <craftworks.jp+dev@gmail.com>
2014-06-06 17:16:12 +0900

    changed numrows supports accessor class

--- lib/Kurious/Interface/DBI.pm
+++ lib/Kurious/Interface/DBI.pm
@@ -3,8 +3,6 @@ package Kurious::Interface::DBI;
 use Kurious::Base 'Kurious::Interface';
 use Carp;
 
-has 'numrows';
-
 sub txn {
     my ($self, $code) = @_;
 
@@ -18,6 +16,26 @@ sub txn {
         $self->log->error($errstr);
         local $Carp::CarpLevel = 2;
         confess;
+    }
+
+    return $rows;
+}
+
+sub numrows {
+    my ($self, $stuff) = @_;
+
+    state $rows;
+
+    return $rows unless defined $stuff;
+
+    if ( $stuff =~ /^\d+$/o ) {
+        $rows = $stuff;
+    }
+    elsif ( $stuff->isa('Kurious::Accessor') ) {
+        $rows = $stuff->found_rows;
+    }
+    else {
+        Carp::croak(qq{$stuff isn't a 'Kurious::Accessor'});
     }
 
     return $rows;
