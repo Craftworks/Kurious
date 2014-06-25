b1e8e1afc86ef8bd5ee59f4276507b9ba22bc7e1
Craftworks <craftworks.jp+dev@gmail.com>
2014-06-25 18:12:58 +0900

    added utility function

--- lib/Kurious/Util.pm
+++ lib/Kurious/Util.pm
@@ -3,7 +3,7 @@ package Kurious::Util;
 use Mojo::Base -strict;
 use Exporter::Lite;
 
-our @EXPORT = qw(rows2kv);
+our @EXPORT = qw(rows2kv rows2krows);
 
 sub rows2kv {
     my ($rows, $key) = @_;
@@ -16,6 +16,22 @@ sub rows2kv {
     }
 
     return +{ map { $_->{ $key } => $_ } @$rows };
+}
+
+sub rows2krows {
+    my ($rows, $key) = @_;
+
+    unless ( ref $rows eq 'ARRAY' ) {
+        Carp::croak('rows is not an ARRAY reference');
+    }
+    unless ( exists $rows->[0]{ $key } ) {
+        Carp::croak(qq{key "$key" is not exists});
+    }
+
+    my %krows;
+    push @{ $krows{ $_->{ $key } } }, $_ for @$rows;
+
+    return \%krows;
 }
 
 1;
